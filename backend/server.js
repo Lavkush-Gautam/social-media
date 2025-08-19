6fad04758c128fa2b6e9d79f8641c4d8966c0b65
lavkush gautam <154265093+Lavkush-Gautam@users.noreply.github.com>
2025-08-19 09:51:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,22 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDb from './config/db.js'
-import { serve } from "inngest/express"
-import { inngest,functions } from './inngest/index.js'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDb from "./config/db.js";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./src/inngest.js";
 
-const app = express()
+const app = express();
 
-await connectDb()
+await connectDb();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
-app.get('/', (req, res) => {
-    res.send('Srever is running')
-})
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-const PORT = process.env.PORT || 4000
-
-app.listen(PORT, () => {
-    console.log(`server is running on PORT ${PORT}`)
-})
-
+// 👇 Export, don't listen
+export default app;
